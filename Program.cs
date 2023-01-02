9c3996095ec568ac9af572e959c4b1ff52ee5127
Dmitrii.Kazantsev <dmitry.kazantsev@jetbrains.com>
2023-01-02 12:16:31 +0200

    asda

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+
+var allRecources = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+foreach (var s in allRecources)
+{
+    Console.WriteLine(s);
+}
